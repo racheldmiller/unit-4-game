748be271fa412c9a10f173f90ed4e960561422f3
racheldmiller <rachelmiller415@gmail.com>
2019-04-05 09:28:22 -0700

    updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,114 @@
 // ========================== GLOBAL VARIABLES ================================ 
 
 // define the gems
-    var gems = [1,2,3,4]
+    var gems = {
+        red: {
+            name: "RED",
+            value: 0
+        },
+        // gems object and inside that object, we have different properties. The properties have values.
+        blue: {
+            name: "BLUE",
+            value: 0
+        },
+        green: {
+            name: "GREEN",
+            value: 0
+        },
+        yellow: {
+            name: "YELLOW",
+            value: 0,
+        },
+    };
+
+console.log(gems.red.name)
+
 
 // define the counters for wins and losses 
     var wins = 0;
     var losses = 0;
 
+// additional variables listed in HTML
+    var scoreNum = 0;
+    var targetScore = 0;
+
 // ========================== HELPER FUNCTIONS ================================
 
-// create function setUp(), which will hold all of the code below.
-function setUp(){
+// Load all the HTML first before anything
+// $(document).ready(function() {
+
     // random number shown at start of game should be between 19 - 120.
+    var Random=Math.floor(Math.random()*101+19)
 
     // crystals should have a random hidden value between 1 - 12.
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
+// });
 
-    // Update HTML for wins and losses after each round. 
-    document.getElementById("wins").innerHTML = wins; 
-    document.getElementById("losses").innerHTML = losses;
+    var random=function(min,max) {
+        return Math.floor(Math.random()*(max-min+1))+min
+    }
+
+    var startGame=function() {
+        scoreNum=0;
+        targetScore=random(19,120)
+           gems.blue.value=random(1,12)
+           gems.red.value=random(1,12)
+           gems.green.value=random(1,12)
+           gems.yellow.value=random(1,12)
+           console.log("yellow" + gems.yellow.value)
+           $("#randomNum").text(targetScore)
+        };
+        
+    // add number function 
+    
+   // ** What is .text in this case, and is it important? **
+    // $("#wins").text(wins);
+    // $("#losses").text(losses);
+
+ // This time, our click event applies to every single crystal on the page. Not just one.
+ 
+
+ function winner(){
+    wins++; 
+    $("#numberWins").text(wins);
+    reset();
+    }
+
+
+ $("#red").on ("click", function(){
+    userTotal = userTotal + num1;
+    console.log("New userTotal= " + userTotal);
+    $("#scoreNum").text(userTotal); 
+
+        if (userTotal == Random){
+          yay();
+        }
+        else if ( userTotal > Random){
+          loser();
+        }   
+  })  
 
     // ========================== MAIN PROCESS =====================================
+startGame()
 
-    // call on the setup function I created under HELPER FUNCTIONS 
-    setup();
+    $(".gemImage").on("click", function() {
+        
+        // 4 individual functions for crystals 
+        // then call on the add value function 
 
-    // capture the key clicks 
-    document.onkeyup = function(event) { 
-
-        // will need to capture other important functionality here.
-    }
-};
-
+    
+        // All of the same game win-lose logic applies. So the rest remains unchanged.
+        alert("New score: " + counter);
+     
+        if (counter === targetNumber) {
+          alert("You win!");
+        }
+     
+        else if (counter >= targetNumber) {
+          alert("You lose!!");
+        }
+     
+      });
