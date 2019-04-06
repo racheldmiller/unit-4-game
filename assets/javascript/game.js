037965d65dd94e445af9e5a28b05b2dbb453fd4c
racheldmiller <rachelmiller415@gmail.com>
2019-04-06 09:28:35 -0700

    removed bugs I found

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,13 +23,12 @@
 
 console.log(gems.red.name)
 
-
 // define the counters for wins and losses 
     var wins = 0;
     var losses = 0;
 
 // additional variables listed in HTML
-    var scoreNum = 0;
+    var userTotal = 0;
     var targetScore = 0;
 
 // ========================== HELPER FUNCTIONS ================================
@@ -38,13 +37,13 @@ console.log(gems.red.name)
 // $(document).ready(function() {
 
     // random number shown at start of game should be between 19 - 120.
-    var Random=Math.floor(Math.random()*101+19)
+    var Random=Math.floor(Math.random()*101+19);
 
     // crystals should have a random hidden value between 1 - 12.
-    var num1= Math.floor(Math.random()*11+1)
-    var num2= Math.floor(Math.random()*11+1)
-    var num3= Math.floor(Math.random()*11+1)
-    var num4= Math.floor(Math.random()*11+1)
+    gems.red.value= Math.floor(Math.random()*11+1);
+    gems.yellow.value= Math.floor(Math.random()*11+1);
+    gems.green.value= Math.floor(Math.random()*11+1);
+    gems.blue.value= Math.floor(Math.random()*11+1);
 // });
 
     var random=function(min,max) {
@@ -52,63 +51,93 @@ console.log(gems.red.name)
     }
 
     var startGame=function() {
-        scoreNum=0;
+        totalScore=0;
         targetScore=random(19,120)
-           gems.blue.value=random(1,12)
-           gems.red.value=random(1,12)
-           gems.green.value=random(1,12)
-           gems.yellow.value=random(1,12)
+        //    gems.blue.value=random(1,12)
+        //    gems.red.value=random(1,12)
+        //    gems.green.value=random(1,12)
+        //    gems.yellow.value=random(1,12)
            console.log("yellow" + gems.yellow.value)
            $("#randomNum").text(targetScore)
         };
-        
-    // add number function 
-    
-   // ** What is .text in this case, and is it important? **
-    // $("#wins").text(wins);
-    // $("#losses").text(losses);
-
- // This time, our click event applies to every single crystal on the page. Not just one.
  
-
  function winner(){
     wins++; 
-    $("#numberWins").text(wins);
+    $("#wins").text(wins);
     reset();
     }
 
-
- $("#red").on ("click", function(){
-    userTotal = userTotal + num1;
-    console.log("New userTotal= " + userTotal);
-    $("#scoreNum").text(userTotal); 
-
-        if (userTotal == Random){
-          yay();
-        }
-        else if ( userTotal > Random){
-          loser();
-        }   
-  })  
+function loser(){
+    losses++;
+    console.log(losses)
+    $("#losses").text(losses);
+    reset();
+    }   
 
     // ========================== MAIN PROCESS =====================================
 startGame()
 
-    $(".gemImage").on("click", function() {
+    // $(".gemImage").on("click", function() {
         
         // 4 individual functions for crystals 
-        // then call on the add value function 
 
+        $("#red").on ("click", function() {
+            userTotal = userTotal + gems.red.value;
+            console.log("New userTotal= " + userTotal);
+            $("#userTotal").text(userTotal); 
+        
+                if (userTotal == targetScore){
+                  winner();
+                }
+                else if ( userTotal > targetScore){
+                  loser();
+                }   
+          })  
+
+          $("#blue").on ("click", function() {
+            userTotal = userTotal + gems.blue.value;
+            console.log("New userTotal= " + userTotal);
+            $("#userTotal").text(userTotal); 
+        
+                if (userTotal == targetScore){
+                  winner();
+                }
+                else if ( userTotal > targetScore){
+                  loser();
+                }   
+          })  
+
+          $("#yellow").on ("click", function() {
+            userTotal = userTotal + gems.yellow.value;
+            console.log("New userTotal= " + userTotal);
+            $("#userTotal").text(userTotal); 
+        
+                if (userTotal == targetScore){
+                  winner();
+                }
+                else if ( userTotal > targetScore){
+                  loser();
+                }   
+          })  
+
+          $("#green").on ("click", function() {
+            userTotal = userTotal + gems.green.value;
+            console.log("New userTotal= " + userTotal);
+            $("#userTotal").text(userTotal); 
+        
+                if (userTotal == targetScore){
+                  winner();
+                }
+                else if ( userTotal > targetScore){
+                  loser();
+                }   
+          })
     
-        // All of the same game win-lose logic applies. So the rest remains unchanged.
-        alert("New score: " + counter);
-     
-        if (counter === targetNumber) {
-          alert("You win!");
-        }
-     
-        else if (counter >= targetNumber) {
-          alert("You lose!!");
-        }
-     
-      });
+        // });
+    
+    function reset() {
+        userTotal= 0;
+        $("#targetScore").text(userTotal);
+        startGame();
+    }
+      
